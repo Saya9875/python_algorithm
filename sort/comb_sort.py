2adb2dc13761edc4c499c496b1a9794f35ea3e49
Saya9875 <amsh222vegan@gmail.com>
2024-06-09 23:13:16 +0900

    fix comb_sort

--- sort/comb_sort.py
+++ sort/comb_sort.py
@@ -1,17 +1,32 @@
+import random
+import time
+
+
 def comb_sort(numbers: list[int]) -> list[int]:
     """
-    O(n**2)
+    Avarage O(n**2/2**p) where p is the number of increments
     """
-    len_numbers = len(numbers)
-    gap = len_numbers
+    numbers_count = len(numbers)
+    gap = numbers_count
     swapped = True
     while gap != 1 or swapped:
         gap = int(gap / 1.3)
         if gap < 1:
             gap = 1
+
         swapped = False
-        for i in range(len_numbers - gap):
+
+        for i in range(0, numbers_count - gap):
             if numbers[i] > numbers[i + gap]:
                 numbers[i], numbers[i + gap] = numbers[i + gap], numbers[i]
                 swapped = True
+
     return numbers
+
+
+ramdom_int_list = random.sample(range(1, 11), 10)
+
+start_time = time.time()
+print(comb_sort(ramdom_int_list))
+end_time = time.time()
+print("Processing time: ", end_time - start_time, "seconds")
